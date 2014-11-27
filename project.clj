bf24f2bafc5984d0fc16790f7f9c09a54a46dd0d
Dmitry Kasimtsev <dmitry.kasimtsev@strikead.com>
2014-11-27 14:01:51 +0200

    set sign-releases to false

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject kasyaar-http-kit/lein-template "0.12.0"
   :description "Template for http-kit, compojure simple projects"
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo" :sign-releases false}]]
   :url "http://github.com/kasyaar/http-kit-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
